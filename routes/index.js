84c0eec27295fa02132b2933c9baa9afec760e42
imarkwick <izzymarkwick@gmail.com>
2015-05-01 11:10:07 +0100

    get from and post to mongo database

--- routes/index.js
+++ routes/index.js
@@ -27,4 +27,51 @@ router.get('/newuser', function(req, res) {
 	res.render('newuser', { title: 'Add New User' })
 });
 
+/* POST to add a new user */
+router.post('/adduser', function(req, res) {
+
+	// set internal DB variable
+	var db = req.db;
+
+	// get form values (relying on 'name' attributes)
+	var userName = req.body.username;
+	var userEmail = req.body.username;
+
+	// set ythe collection
+	var collection = db.get('usercollection');
+
+	// submit to the database
+	collection.insert({
+		"username" : userName,
+		"email" : userEmail
+	}, function(err, doc) {
+		if (err) {
+			// return error if it fails
+			res.send("There was a problem adding your information to the database.")
+		}
+		else {
+			// if successful, remove /adduser from the headbar
+			res.location("userlist");
+			// and forward to the success page
+			res.redirect("userlist");
+		}
+	});
+});
+
+
+
+
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
